4389eb27a5efaec64ec5a26097b4590dd411f437
Alexis Burgos <alexis.burgos@showroomprive.net>
2023-12-06 10:52:49 +0100

    update

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Nexus",
-            url: "https://github.com/alexis-burgos/xcode-spm-test/Nexus-unspecified.zip",
-            checksum: "bde1c69418ac94f808a9a332ade7f224ea738fd1123a2aa0fcbc115fd2dd86a0"
+            path: "./Nexus.xcframework"
         ),
     ]
 )
