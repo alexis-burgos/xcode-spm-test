ec38da635ce04f51ca3121d34f0e0852793a9957
Alexis Burgos <alexis.burgos@showroomprive.net>
2023-12-08 16:09:08 +0100

    update

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Nexus",
-            url: "https://github.com/alexis-burgos/xcode-spm-test/releases/download/0.0.1-SNAPSHOT/Nexus-unspecified.zip",
-            checksum: "d5bdbc892ebe72bc31d620f9f052eadd7e7ab105d65f56cdbc97153110c345c0"
+            url: "https://github.com/alexis-burgos/xcode-spm-test/releases/download/0.0.1-SNAPSHOT/Nexus-0.0.1-SNAPSHOT.zip",
+            checksum: "8a353df8821fabb1e2177729dfb6e7fd82736892a306b1cdfec74f24125e1d38"
         ),
     ]
 )
