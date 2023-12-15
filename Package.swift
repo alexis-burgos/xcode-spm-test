af5dd75a7f21c6375e0ca8c7917243210f340d87
Alexis Burgos <alexis.burgos@showroomprive.net>
2023-12-15 09:05:43 +0100

    core: update build.gradle.kts
    Publish all android variant

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "Nexus",
             url: "https://github.com/alexis-burgos/xcode-spm-test/releases/download/0.0.1-SNAPSHOT/Nexus-0.0.1-SNAPSHOT.zip",
-            checksum: "6d71bbf312dbf483c635b8a1203e8a0a9bcb0b949a9d7096d4e3dc7564de82f7"
+            checksum: "32ebeb7a26b3dfb1132513298655b4516c437ab236e10f06438c396afafe17f6"
         ),
     ]
 )
