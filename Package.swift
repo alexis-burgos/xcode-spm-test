077dea82b45f0b896b05b5a9aeee0728900c87c3
Alexis Burgos <alexis.burgos@showroomprive.net>
2023-12-20 12:18:50 +0100

    core: Completely revamp the configuration
    Make a huge rework of the Nexus configuration:
     - Targets has been adjusted: no more Android target needed, just Jvm.
     - Publication is cleaner
     - Unnecessary classes has been removed until we really needed them

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "Nexus",
             url: "https://github.com/alexis-burgos/xcode-spm-test/releases/download/0.0.1-SNAPSHOT/Nexus-0.0.1-SNAPSHOT.zip",
-            checksum: "32ebeb7a26b3dfb1132513298655b4516c437ab236e10f06438c396afafe17f6"
+            checksum: "082834a89185e66fe933ecec5cd748110e1deabe0a5b5b781cdbd0509732d808"
         ),
     ]
 )
